884a04d4f43433df4649f4416f927d1cdf57e445
Jim Thoburn <jim@jimthoburn.com>
2019-07-18 14:43:20 -0700

    Fix example

--- main.js
+++ main.js
@@ -8,21 +8,30 @@ let stringify = require('csv-stringify');
 
 
 // Load the CSV data
-
 // https://data.lacounty.gov/Geospatial/ZIP-Codes/65v5-jw9f/data
 let zipCodesLACounty = parse(fs.readFileSync('data/zip-codes-la-county.csv', 'utf8'), {columns: true});
 
 
-// Create a new data set with some useful results
+// Create a new data set for the results
 let results = [];
 
 
+// Add some useful data
+results.push(["value for column a", "value for column b"]);
+
+
+// Loop through CSV data
+zipCodesLACounty.forEach(function(row) {
+  // Do something useful
+});
+
+
 // Save the data a new spreadsheet
-let resultsColumnNames = ["Column A", "Column B"];
-let csvArray = [];
+let resultsColumnNames = [["Column A", "Column B"]];
+let csvArray = resultsColumnNames.concat(results);
+console.dir(csvArray);
 let csvWritePath = "./data-generated";
 let csvFileName = "results.csv";
-csvArray.push(resultsColumnNames.concat(results));
 stringify(csvArray, function(err, output) {
   mkdirp(csvWritePath, function (err) {
     if (err) {
